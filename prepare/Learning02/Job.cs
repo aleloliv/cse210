1d20fec424c4a6c8869b1d31a5475ba76c009f5b
aleloliv <aleloliv555@hotmail.com>
2024-01-16 07:19:21 -0300

    Week2 Learning02 assignment

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Job
+{
+    public string _company; //{ get; set; }
+    public string _jobTitle; // { get; set; }
+    public int _startYear; // { get; set; }
+    public int _endYear; // { get; set; }
+
+    public Job() // empty constructor
+    {
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
