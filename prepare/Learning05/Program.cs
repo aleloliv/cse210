351db332b935732c5bf04682f46fba13c2262a58
aleloliv <aleloliv555@hotmail.com>
2024-02-05 12:11:06 -0300

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 2.0);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("blue", 2.0, 3.0);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("yellow", 3.0);
+        shapes.Add(circle);
+        
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.Write("Color: " + color + ", ");
+            Console.WriteLine("Area: " + area.ToString("F2"), CultureInfo.InvariantCulture);
+        }
     }
 }
